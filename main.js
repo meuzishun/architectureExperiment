408726cbe425a91d70b0b2629064622f37aa1b1a
Andrew Smith <meuzishun@gmail.com>
2022-03-09 12:48:25 -0500

    write code to render addProgrammerForm

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
 import { Person } from './js/modules/person.js';
 import { Programmer } from './js/modules/programmer.js';
+import { UI } from './js/UI.js';
 
-const andrew = new Person('Andrew', 'Smith', 40);
-
-const morgan = new Programmer(
-  'Morgan',
-  'Hodges',
-  38,
-  ['JavaScript', 'Node', 'PHP', 'Python'],
-  15
-);
-
-console.log(morgan.getLanguages());
-morgan.addLanguage('HTML');
-console.log(morgan.getLanguages());
-
-console.log(morgan.getAge());
-morgan.ageOneYear();
-console.log(morgan.getAge());
+UI.renderNewProgrammerForm();
